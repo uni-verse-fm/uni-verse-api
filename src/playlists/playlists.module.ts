b85b502d880ba1cc5e7347a42d5872cb067cfb73
abdelillah <aghomari.professionnel@gmail.com>
2022-04-11 00:30:57 +0200

    add missing dependencies to list module

--- src/playlists/playlists.module.ts
+++ src/playlists/playlists.module.ts
@@ -3,13 +3,20 @@ import { PlaylistsService } from './playlists.service';
 import { PlaylistsController } from './playlists.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Playlist, PlaylistSchema } from './schemas/playlist.schema';
+import { TracksService } from '../tracks/tracks.service';
+import { Track, TrackSchema } from '../tracks/schemas/track.schema';
+import { FilesService } from '../files/files.service';
+import { UsersService } from '../users/users.service';
+import { User, UserSchema } from '../users/schemas/user.schema';
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Playlist.name, schema: PlaylistSchema },
+      { name: Track.name, schema: TrackSchema },
+      { name: User.name, schema: UserSchema },
     ]),
   ],
   controllers: [PlaylistsController],
-  providers: [PlaylistsService],
+  providers: [PlaylistsService, TracksService, FilesService, UsersService],
 })
 export class PlaylistsModule {}
