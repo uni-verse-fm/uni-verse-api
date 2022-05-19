bdbe279adf4f9062c7d2b87a898da1a8149f7957
abdelillah <aghomari.professionnel@gmail.com>
2022-05-20 01:46:34 +0200

    fix update playlist

--- src/playlists/dto/update-playlist.dto.ts
+++ src/playlists/dto/update-playlist.dto.ts
@@ -1,25 +1,31 @@
 import { PartialType } from '@nestjs/swagger';
 import {
   IsEnum,
+  IsNotEmpty,
   IsOptional,
   IsString,
   MaxLength,
   MinLength,
 } from 'class-validator';
-import { CreatePlaylistDto } from './create-playlist.dto';
 
 export enum PlaylistUpdateTaskAction {
   Remove = 'REMOVE',
   Add = 'ADD',
 }
-export class UpdatePlaylistDto extends PartialType(CreatePlaylistDto) {
+export class UpdatePlaylistDto {
   @IsString()
   @IsOptional()
   @MinLength(1)
   @MaxLength(50)
-  readonly trackId: string;
+  readonly trackId?: string;
 
   @IsEnum(PlaylistUpdateTaskAction)
   @IsOptional()
   readonly action?: PlaylistUpdateTaskAction;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(1)
+  @MaxLength(50)
+  readonly title?: string;
 }
