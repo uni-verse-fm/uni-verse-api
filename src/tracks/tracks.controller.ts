5803546383bf7e7c478068cb72da403b6bcd4646
Yoni FIRROLONI <firroloni.yoni@gmail.com>
2022-07-06 00:29:11 +0200

    add specific route for plagia

--- src/tracks/tracks.controller.ts
+++ src/tracks/tracks.controller.ts
@@ -31,7 +31,7 @@ export class TracksController {
     return [];
   }
 
-  @Patch(':id')
+  @Patch('/plagia/:id')
   @ApiCookieAuth('Set-Cookie')
   @ApiOperation({ summary: 'INTERNAL: updates a fingerprint search.' })
   @UseInterceptors(FpSearchInterceptor, ValidIdInterceptor)
